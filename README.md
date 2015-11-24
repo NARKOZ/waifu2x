3c76cc65d68a918804cc494e4cce8d9cc17407ef
Nihad Abbasov <narkoz.2008@gmail.com>
2015-11-24 16:09:53 +0400

    allow to set source image from URL

--- README.md
+++ README.md
@@ -30,6 +30,9 @@ bundle install
 # convert image.png with 2x upscaling
 Waifu2x.convert('image.png', nil, { scale: 2 })
 
+# convert image.png from remote url
+Waifu2x.convert('http://example.net/image.png', nil, { scale: 2 })
+
 # convert image.png to converted.png without noise reduction and with 2x upscaling
 Waifu2x.convert('image.png', 'converted.png', { noise: 0, scale: 2 })
 ```
@@ -39,6 +42,9 @@ Waifu2x.convert('image.png', 'converted.png', { noise: 0, scale: 2 })
 ```sh
 # convert sample.jpg with 2x upscaling
 waifu2x sample.jpg -s 2
+
+# convert sample.jpg from remote url
+waifu2x http://example.net/sample.jpg -s 2
 
 # convert sample.jpg to output.jpg without noise reduction and with 2x upscaling
 waifu2x sample.jpg output.jpg -n 0 -s 2
--- lib/waifu2x.rb
+++ lib/waifu2x.rb
@@ -6,38 +6,47 @@ module Waifu2x
   USER_AGENT = "Waifu2x Ruby Gem #{Waifu2x::VERSION}".freeze
   API_ENDPOINT = 'http://waifu2x.udp.jp/api'.freeze
 
-  # Creates a new image
+  # Converts image from a local file or URL
   #
-  # @param  [String] image Image filename to convert
-  # @param  [String, nil] filename Output image filename with extension
+  # @param  [String] source Source of image to convert (url or filename)
+  # @param  [String, nil] output_file Name of output image file (with extension)
   # @param  [Hash] options
   # @option options [Integer] :noise Noise reduction (0 - none, 1 - low, 2 - high), default: 2
   # @option options [Integer] :scale Upscaling (0 - none, 1 - 1.6x, 2 - 2x), default: 0
-  # @return [String] Output image filename
-  def self.convert(image, filename=nil, options={})
+  # @option options [Boolean] :raw When true will return a converted image in raw data (binary)
+  # @return [String] converted image in raw data (binary) or output filename
+  def self.convert(source, output_file=nil, options={})
     noise = options[:noise] || 2
     scale = options[:scale] || 0
 
     validate_options! noise, scale
 
-    begin
-      file = File.open(image)
-    rescue
-      raise InvalidImage, "Can't open image file: #{image}"
+    if source.start_with?('http', 'ftp')
+      source_params = { url: source }
+    else
+      begin
+        source_params = { file: File.open(source) }
+      rescue
+        raise InvalidImage, "Can't open image file: #{source}"
+      end
     end
 
     response = Typhoeus.post(API_ENDPOINT,
       headers: { 'User-Agent' => USER_AGENT },
-      body: { noise: noise.to_s, scale: scale.to_s, file: file }
+      body: { noise: noise, scale: scale }.merge(source_params)
     )
 
-    raise InvalidImage, response.body if response.body == 'ERROR: unsupported image format.'
+    raise InvalidImage, response.body if response.body.include?('ERROR')
     raise Waifu2x::ServerError, "Request to Waifu2x API failed with response code: #{response.code}" if response.code != 200
 
-    filename = output_filename(file) if filename.to_s.strip.empty?
+    output_file = output_filename(file) if output_file.to_s.strip.empty?
 
-    File.write(filename, response.body)
-    filename
+    if options[:raw]
+      response.body
+    else
+      File.write(output_file, response.body)
+      output_file
+    end
   end
 
   # @private
